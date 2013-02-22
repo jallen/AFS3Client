baf7cb8110003dff47fdf3b92c82532215b70131
DhilipSiva Bijju <dhilipsiva@gmail.com>
2013-02-22 20:41:54 +0530

    Enable arc

--- AFS3Client.m
+++ AFS3Client.m
@@ -11,10 +11,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -61,18 +61,13 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 }
 
 - (void)dealloc {
-	[_accessKey release];
-	[_secretAccessKey release];
-	[_sessionToken release];
-	[_accessPolicy release];
-	[super dealloc];
 }
 
-- (void)putObjectForData:(NSData *)data 
-							withBucket:(NSString *)bucket 
-										 key:(NSString *)key
-								 success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
-								 failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
+- (void)putObjectForData:(NSData *)data
+              withBucket:(NSString *)bucket
+                     key:(NSString *)key
+                 success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
+                 failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
 {
 	NSString *path = [NSString stringWithFormat:@"%@%@", bucket, key];
 	
@@ -88,9 +83,9 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 	}];
 }
 
-#pragma mark - AFHTTPClient 
+#pragma mark - AFHTTPClient
 
-- (void)putPath:(NSString *)path 
+- (void)putPath:(NSString *)path
            data:(NSData *)data
         success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
         failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
@@ -100,9 +95,9 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 	[self enqueueHTTPRequestOperation:operation];
 }
 
-- (NSMutableURLRequest *)requestWithMethod:(NSString *)method 
-																			path:(NSString *)path 
-																			data:(NSData *)data
+- (NSMutableURLRequest *)requestWithMethod:(NSString *)method
+                                      path:(NSString *)path
+                                      data:(NSData *)data
 {
 	NSMutableURLRequest *request = [super requestWithMethod:method path:path parameters:nil];
 	[request setHTTPBody:data];
@@ -133,8 +128,8 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 	NSString *authorizationString = [NSString stringWithFormat:@"AWS %@:%@", _accessKey, signature];
 	[self setDefaultHeader:@"Authorization" value:authorizationString];
 }
-	 
-	 
+
+
 - (NSMutableDictionary *)S3Headers {
 	NSMutableDictionary *headers = [NSMutableDictionary dictionary];
 	if (_accessPolicy) {
@@ -145,14 +140,14 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 	}
 	return headers;
 }
-	 
+
 #pragma mark - Helper Methods
 
 + (NSString *)stringByURLEncodingForS3Path:(NSString *)key {
 	if (!key) {
 		return @"/";
 	}
-	NSString *path = [(NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)key, NULL, CFSTR(":?#[]@!$ &'()*+,;=\"<>%{}|\\^~`"), CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding)) autorelease];
+	NSString *path = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)key, NULL, CFSTR(":?#[]@!$ &'()*+,;=\"<>%{}|\\^~`"), CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding)));
 	if (![[path substringWithRange:NSMakeRange(0, 1)] isEqualToString:@"/"]) {
 		path = [@"/" stringByAppendingString:path];
 	}
@@ -167,8 +162,8 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 	NSMutableDictionary *threadDict = [[NSThread currentThread] threadDictionary];
 	NSDateFormatter *dateFormatter = [threadDict objectForKey:@"ASIS3ResponseDateFormatter"];
 	if (dateFormatter == nil) {
-		dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
-		[dateFormatter setLocale:[[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"] autorelease]];
+		dateFormatter = [[NSDateFormatter alloc] init];
+		[dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"] ];
 		[dateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
 		[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'.000Z'"];
 		[threadDict setObject:dateFormatter forKey:@"ASIS3ResponseDateFormatter"];
@@ -180,9 +175,9 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 	NSMutableDictionary *threadDict = [[NSThread currentThread] threadDictionary];
 	NSDateFormatter *dateFormatter = [threadDict objectForKey:@"ASIS3RequestHeaderDateFormatter"];
 	if (dateFormatter == nil) {
-		dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
+		dateFormatter = [[NSDateFormatter alloc] init];
 		// Prevent problems with dates generated by other locales (tip from: http://rel.me/t/date/)
-		[dateFormatter setLocale:[[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"] autorelease]];
+		[dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"] ];
 		[dateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
 		[dateFormatter setDateFormat:@"EEE, d MMM yyyy HH:mm:ss Z"];
 		[threadDict setObject:dateFormatter forKey:@"ASIS3RequestHeaderDateFormatter"];
@@ -219,7 +214,7 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 		output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
 	}
 	
-	return [[[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding] autorelease];
+	return [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
 }
 
 // From: http://stackoverflow.com/questions/476455/is-there-a-library-for-iphone-to-work-with-hmac-sha-1-encoding
@@ -239,17 +234,18 @@ NSString *const AFIS3AccessPolicyBucketOwnerFullControl = @"bucket-owner-full-co
 }
 
 + (NSString *)mimeTypeForFileAtPath:(NSString *)path {
-	if (![[[[NSFileManager alloc] init] autorelease] fileExistsAtPath:path]) {
+	if (![[[NSFileManager alloc] init]fileExistsAtPath:path]) {
 		return nil;
 	}
 	// Borrowed from http://stackoverflow.com/questions/2439020/wheres-the-iphone-mime-type-database
-	CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (CFStringRef)[path pathExtension], NULL);
+	CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)[path pathExtension], NULL);
 	CFStringRef MIMEType = UTTypeCopyPreferredTagWithClass (UTI, kUTTagClassMIMEType);
+    NSString *mimeType = (__bridge_transfer NSString *)(MIMEType);
 	CFRelease(UTI);
 	if (!MIMEType) {
 		return @"application/octet-stream";
 	}
-	return [NSMakeCollectable(MIMEType) autorelease];
+	return mimeType;
 }
 
 
